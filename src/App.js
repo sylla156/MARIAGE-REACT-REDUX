debc9c186f8bd5ea60d5595b6f66fc1d93f1d302
sylla ibrahim <ibrahimsyllac196@gmail.com>
2022-06-23 10:47:12 +0000

    FEATURE(Home): Add home background image

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
+import Home from './Pages/Home/Home';
 
 function App() {
   return (
-   <div className='App'> 
-   <h1>hello world</h1>
-   </div>
+  <Home/>
   );
 }
 
