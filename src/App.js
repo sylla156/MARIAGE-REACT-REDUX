6fe152600b476c380351b8708c262a88e6427c8b
sylla ibrahim <ibrahimsyllac196@gmail.com>
2022-06-25 18:02:41 +0000

    FEATURE: add loader for dasbord

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import Alert from './Helpers/Alerts/Alerts';
+import Loader from './Helpers/Loader/Loader';
 
 function App() {
   return (
-   <Alert method='success' title='connexion' message='connexion etablir avec success'/>
+  //  <Alert method='success' title='connexion' message='connexion etablir avec success'/>
+  <Loader/>
   );
 }
 
