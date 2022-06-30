f3f4438cd80879898a6fadf19f5a4fe789aaa25d
sylla ibrahim <ibrahimsyllac196@gmail.com>
2022-06-30 09:39:35 +0000

    REFACTOR(dashboard): switch  in different file

---
+++ src/Components/DashboardCenter/DashboardCenter.jsx
@@ -0,0 +1,246 @@
+import React from "react";
+import cr from "../../Assets/img/Icons/cr.png";
+import search from "../../Assets/img/Profils/search.png";
+import cloche from "../../Assets/img/Profils/cloche.png";
+import logout from "../../Assets/img/Profils/logout.png";
+import profile from "../../Assets/img/Profils/profile.png";
+import calend from "../../Assets/img/Profils/calend.png";
+import book from "../../Assets/img/Profils/book.png";
+import people from "../../Assets/img/Profils/people.png";
+import position from "../../Assets/img/Profils/position.png";
+import otherUser from "../../Assets/img/Profils/otherUser.png";
+import invitation from "../../Assets/img/Profils/invitation.png";
+import creation from "../../Assets/img/Profils/creation.png";
+import tableImg from "../../Assets/img/Profils/tableImg.png";
+import imgAction from "../../Assets/img/Profils/imgAction.png";
+import './dashboardCenter.scss';
+
+const DashboardCenter = () => {
+  return (
+    <div className="dashboard__center">
+      {/* navbar top por setting et search of app */}
+      <Navbar />
+      {/* where the data will be represente  */}
+      <DashboardCenterData />
+    </div>
+  );
+};
+
+const DashboardCenterData = () => {
+  return (
+    <div className="dashboard__center--data">
+      <Header />
+      <TableTitle />
+      <TableContent />
+    </div>
+  );
+};
+const TableTitle = () => {
+  return (
+    <div className="tableTitle">
+      <div className="title">
+        <div className="subtitle">
+          <p>liste des invites (45)</p>
+          <p>groupe d'invites (03)</p>
+          <p>programmes (01)</p>
+        </div>
+        <div className="stats">
+          <p>
+            <span>34</span> invitations en attentes
+          </p>
+        </div>
+      </div>
+      <div className="border">
+        <div className="first"></div>
+        <div className="second"></div>
+        <div className="three"></div>
+      </div>
+      <div className="search">
+        <div className="inputs">
+          <div className="status">
+            <label htmlFor="status">Trier par</label>
+            <select name="status" id="status">
+              <option value="">Confimees</option>
+              <option value="dog">Dog</option>
+            </select>
+          </div>
+          <div className="tags">
+            <label htmlFor="tags">tags</label>
+            <select name="tags" id="tags">
+              <option value="">parents</option>
+              <option value="dog">vip</option>
+            </select>
+          </div>
+        </div>
+        <div className="btn">
+          <p>Action selection</p>
+          <p>
+            <img src={creation} alt="creation de groupe" />
+            <span>cree un groupe</span>
+          </p>
+          <p>
+            <img src={invitation} alt="envoyer une invitation" />
+            <span>envoyer une invitation</span>
+          </p>
+          <p className="addUser">
+            <img src={otherUser} alt="ajouter un user" />
+            <span>Ajouter</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+const TableContent = () => {
+  return (
+    <div className="tableContent">
+      <table>
+        <thead>
+          <tr>
+            <th>
+              <input type="checkbox" />
+            </th>
+            <th>
+              Nom et prenom <Imgs />
+            </th>
+            <th>
+              Tags <Imgs />
+            </th>
+            <th>
+              Cote/Table <Imgs />
+            </th>
+            <th>
+              Guess <Imgs />
+            </th>
+            <th>
+              Telephone <Imgs />
+            </th>
+            <th>
+              E-mail <Imgs />
+            </th>
+            <th>
+              Identifiant <Imgs />
+            </th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>
+              <input type="checkbox" />
+            </td>
+            <td>Sylla ibrahim</td>
+            <td>
+              <span>Parent</span>
+              <span>vip</span>
+            </td>
+            <td>Parasole Gauche</td>
+            <td>4</td>
+            <td>07080050604</td>
+            <td>brahim@novate.com</td>
+            <td>FF5WE66F</td>
+            <td>
+              <ImgAction />
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <input type="checkbox" />
+            </td>
+            <td>Sylla ibrahim</td>
+            <td>
+              <span>Parent</span>
+              <span>vip</span>
+            </td>
+            <td>Parasole Gauche</td>
+            <td>4</td>
+            <td>07080050604</td>
+            <td>brahim@novate.com</td>
+            <td>FF5WE66F</td>
+            <td>
+              <ImgAction />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+const Imgs = () => {
+  return <img src={tableImg} alt="icons" />;
+};
+const ImgAction = () => {
+  return <img src={imgAction} alt="action" />;
+};
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="first">
+        <DashboardTitle img={cr} title="ceremonie religieuse" />
+        <div className="underline"></div>
+        <div className="details">
+          <div className="premier">
+            <img src={calend} alt="calendrier" />
+            <span>Le 17 Mars 2022</span>
+            <span>A partir de 10 h</span>
+          </div>
+          <div className="deuxieme">
+            <img src={position} alt="position" />
+            <span>Mosquee Hamsa</span>
+            <span>Cocody,Abidjan,Cote d'ivoire</span>
+          </div>
+        </div>
+      </div>
+      <div className="second">
+        <div className="premier child">
+          <div className="title">
+            <span>Programmes</span>
+            <img src={book} alt="book" />
+          </div>
+          <div className="content">
+            <p>03</p>
+          </div>
+        </div>
+        <div className="deuxieme child">
+          <div className="title">
+            <span>Invite</span>
+            <img src={people} alt="people" />
+          </div>
+          <div className="content">
+            <p>03</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const DashboardTitle = (props) => {
+  return (
+    <div className="table">
+      <img src={props.img} alt="tableau de title" />
+      <span>{props.title}</span>
+    </div>
+  );
+};
+
+const Navbar = () => {
+  return (
+    <div className="dashboard__center--navbar">
+      <div className="form">
+        <input type="text" placeholder="input search text" />
+        <button>
+          <img src={search} alt="search" />
+        </button>
+      </div>
+
+      <div className="profils">
+        <img src={cloche} alt="cloche" />
+        <img src={profile} alt="profil" />
+        <p>Admin</p>
+        <img src={logout} alt="logout" />
+      </div>
+    </div>
+  );
+};
+export default DashboardCenter;
