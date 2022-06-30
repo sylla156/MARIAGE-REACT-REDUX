1c5a926891019d63e66a31bdeb04a9271fad2669
sylla ibrahim <ibrahimsyllac196@gmail.com>
2022-06-30 09:53:40 +0000

    REFACTOR: switch file and components folder

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import search from "../../Assets/img/Profils/search.png";
+import cloche from "../../Assets/img/Profils/cloche.png";
+import logout from "../../Assets/img/Profils/logout.png";
+import profile from "../../Assets/img/Profils/profile.png";
+import './navbar.scss';
+const Navbar = () => {
+    return (
+      <div className="dashboard__center--navbar">
+        <div className="form">
+          <input type="text" placeholder="input search text" />
+          <button>
+            <img src={search} alt="search" />
+          </button>
+        </div>
+  
+        <div className="profils">
+          <img src={cloche} alt="cloche" />
+          <img src={profile} alt="profil" />
+          <p>Admin</p>
+          <img src={logout} alt="logout" />
+        </div>
+      </div>
+    );
+  };
+
+export default Navbar
